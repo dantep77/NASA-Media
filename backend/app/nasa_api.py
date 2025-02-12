f5da74a38066b28ad5bee59ff1a93e8e0d456e21
dtparadi <dtparadi@ncsu.edu>
2025-02-12 13:42:02 -0500

    pagination

--- backend/app/nasa_api.py
+++ backend/app/nasa_api.py
@@ -7,9 +7,26 @@ load_dotenv()  # Load API key from .env file
 NASA_API_URL = "https://images-api.nasa.gov/search"
 
 def search_nasa(query):
+    url = NASA_API_URL
     params = {"q": query, "media_type": "image"}
-    response = requests.get(NASA_API_URL, params=params)
-    
+    all_items = []
+    response = requests.get(url, params=params)
+
+    while url:
+        response = response.json()
+
+        #Get items from current page
+        all_items.extend(response["collection"]["items"])
+
+        #Check for another page
+        metadata = response["collection"]["metadata"]
+        if metadata["prompt"] == "Next":
+            url = metadata["href"]
+            response = requests.get(url)
+        else:
+            url = None
+        
+
     if response.status_code == 200:
         return response.json()
     return {"error": "Failed to fetch data from NASA API"}
