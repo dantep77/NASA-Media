383f0a9c3e92bde745d6ba9c98598d18b2f367dd
dtparadi <dtparadi@ncsu.edu>
2025-02-12 14:31:51 -0500

    pagination

--- backend/app/nasa_api.py
+++ backend/app/nasa_api.py
@@ -19,11 +19,9 @@ def search_nasa(query):
         all_items.extend(response["collection"]["items"])
 
         #Check for another page
-        print("response", response)
         links = response.get("collection", {}).get("links", [])
-        print("collection",response.get("collection", {}))
-        print("links", links)
-        if links[0].get("prompt") == "Next":
+    
+        if len(links) > 0:
             url = links[0].get("href")
             response = requests.get(url)
         else:
