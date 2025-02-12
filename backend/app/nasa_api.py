914cbd605ff6018f25a24c7a372ec8e98ecf80e7
dtparadi <dtparadi@ncsu.edu>
2025-02-12 15:47:19 -0500

    typo

--- backend/app/nasa_api.py
+++ backend/app/nasa_api.py
@@ -11,11 +11,11 @@ def search_nasa(query, page):
     url = NASA_API_URL
     params = {"q": query, "media_type": "image", "page": page, "page_size": 12}
     response = requests.get(url, params=params)
-
+    
     if response.status_code == 200:
         return jsonify({
-            response.json()["collection"]["items"],
-            response.json()["collection"]["metadata"]["total_hits"]
+            "items": response.json()["collection"]["items"],
+            "count": response.json()["collection"]["metadata"]["total_hits"]
         })
 
     return {"error": "Failed to fetch data from NASA API"}
