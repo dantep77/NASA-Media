aac7c35ccc64f621a317124342f148c512fb41c4
dtparadi <dtparadi@ncsu.edu>
2025-02-12 12:32:29 -0500

    enabled cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app.routes import api_blueprint
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)  # Enable CORS for all routes
+    
     # Register API routes
     app.register_blueprint(api_blueprint)
 
